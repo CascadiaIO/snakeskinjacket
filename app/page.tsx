2cb5450ae334f29b63c2b139076027644a7a5152
Jason Bettineski <jason@cascadiaio.com>
2025-11-22 08:55:29 -0900

    Revert "Live Layout"

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
-import LiveDesktop from "@/components/live-desktop";
-import LiveMobile from "@/components/live-mobile";
-import OfflineDesktop from "@/components/offline-desktop";
-import OfflineMobile from "@/components/offline-mobile";
-
 export default function Home() {
-  const isLive = process.env.STREAM_LIVE === "true";
   return (
     <main className="flex min-h-screen items-center justify-center font-sans px-4 py-6 md:px-16 md:py-0">
       {/* Mobile foreground image */}
-      <div className="block md:hidden max-w-full h-auto">
-        {isLive ? <LiveMobile /> : <OfflineMobile />}
-      </div>
+      <img
+        src="/bgs/Landing_Mobile.png"
+        alt="Landing"
+        className="block md:hidden max-w-full h-auto"
+      />
       {/* Desktop foreground image */}
-      <div className="hidden md:block max-w-3/4 xl:max-w-3/5 h-auto md:max-h-screen object-contain">
-        {isLive ? <LiveDesktop /> : <OfflineDesktop />}
-      </div>
+      <img
+        src="/bgs/Landing_Desktop.png"
+        alt="Landing"
+        className="hidden md:block max-w-3/4 xl:max-w-3/5 h-auto md:max-h-screen object-contain"
+      />
     </main>
   );
 }
